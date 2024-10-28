ea6bf3cfedc51c7b643ff9f21de9e85e16b61f3d
Onerjg <ierik66@icloud.com>
2024-10-28 17:47:08 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+#Loading the Data Set
+df = pd.read_csv('vehicles_us.csv')
+
+#Adding Header
+st.header("Vehicle Data Dashboard")
+
+#Displaying Data
+st.write("This dashboard shows some exploratory data analysis of the vehicles in the dataset.")
+
+#Adding Histrogram
+fig_histogram = px.histogram(df, x='price', title='Distribution of Vehicle Prices')
+st.plotly_chart(fig_histogram)
+
+# Adding scatter plot
+fig_scatter = px.scatter(df, x='odometer', y='price', title='Odometer vs. Vehicle Price')
+st.plotly_chart(fig_scatter)
+
+# Adding checkbox
+show_expensive_cars = st.checkbox('Show only cars priced above $20,000')
+
+if show_expensive_cars:
+    filtered_df = df[df['price'] > 20000]
+else:
+    filtered_df = df
+
+st.write(filtered_df)
